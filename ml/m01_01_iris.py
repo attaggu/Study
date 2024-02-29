c019eb72d8d3e4ad410049632d0bab97addc767d
attaggu <attagg@naver.com>
2024-02-29 20:52:25 +0900

    0229

--- ml/m01_01_iris.py
+++ ml/m01_01_iris.py
@@ -19,6 +19,9 @@ y=datasets.target
 
 
 
+
+
+
 x_train,x_test,y_train,y_test = train_test_split(x,y,
                                                  test_size=0.2,
                                                  random_state=2727,
